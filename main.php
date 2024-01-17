c7ed024a1827801a017c75696998d0af90881744
TQVLucKy <tranquanvinh882003@gmail.com>
2024-01-17 11:12:59 +0700

    Optimize adding music

--- main.php
+++ main.php
@@ -18,10 +18,13 @@ if (isset($_POST["submit"])) {
     } else {
         echo "Lỗi: " . $sql . "<br>" . $conn->error;
     }
-    
 }
+
+
 $sql = "select * from storemusic";
 $results = mysqli_query($conn, $sql);
+
+
 
 ?>
 
@@ -40,7 +43,7 @@ $results = mysqli_query($conn, $sql);
 
 <body>
     <div class="container-fluid">
-        <div class="container-fluid header border-0 bg-dark">
+        <div class="container-fluid header border-0">
             <div class="header-title">
                 <a class="h1" href="./main.php">Music</a>
             </div>
@@ -58,20 +61,23 @@ $results = mysqli_query($conn, $sql);
                 <a class="DangNhap btn btn-secondary" href="./account/login.php">Đăng Nhập</a>
             </div>
         </div>
-        <form class="container-fluid" action="main.php" method="post" enctype="multipart/form-data">
+        <form class="addmusic" id="showcreate" style="display:none;" class="container-fluid" action="main.php" method="post" enctype="multipart/form-data">
             <label for="image">Chọn hình ảnh:</label>
-            <input type="file" name="image">
+            <input type="file" name="image"></br>
+            <label for="artist">Tên ca sĩ:</label>
             <input type="text" name="artist">
             <input type="submit" name="submit" value="Tải lên">
         </form>
         <div class="container-fluid main row">
             <div class="container-fluid library d-flex col-md-2 pt-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-book-fill " viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-book-fill " viewBox="0 0 16 16">
                     <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
                 </svg>
                 <h2>Thư Viện</h2>
-                <i class="material-icons">add</i>
+                <button class="material-icons" id="show" onclick="Show()">add</button>
+
             </div>
+
             <div class="list col-md-10">
                 <div class="top d-flex justify-content-between mt-2">
                     <h2>Danh sách phát</h2>
@@ -116,6 +122,23 @@ $results = mysqli_query($conn, $sql);
             </div>
         </div>
     </div>
+    <div id="create" style="display: none;">
+        <button id="ShowCreate" onclick="AddMusic()">thêm nhạc</button><br>
+        <button id="ShowCreate" onclick="AddList()">thêm danh sách</button>
+    </div>
 </body>
+
+<script>
+    function Show() {
+        document.getElementById('create').style.display = "block";
+        document.getElementById('create').style.position = "absolute";
+    }
+
+    function AddMusic() {
+        document.getElementById('showcreate').style.display = "block";
+        document.getElementById('showcreate').style.position = "absolute";
+        document.getElementById('showcreate').style.zIndex = "1";
+    }
+</script>
 
 </html>
