e0aa8100c2d0977f3abd06b7b81a7ef0d8c047c7
TQVLucKy <tranquanvinh882003@gmail.com>
2024-01-22 12:07:00 +0700

    addlist

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 // $dirname="./img/";
 // $images= glob($dirname."*.jpg");
 include './connect.php';
-if (isset($_POST["submit"])) {
+if (isset($_POST["submitmusic"])) {
     //đường dẫn tạm thời của tệp hình ảnh đã được gửi lên
     $imageName = addslashes($_FILES["image"]["name"]);
     $imageData = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));
@@ -19,13 +19,24 @@ if (isset($_POST["submit"])) {
         echo "Lỗi: " . $sql . "<br>" . $conn->error;
     }
 }
-
-
 $sql = "select * from storemusic";
 $results = mysqli_query($conn, $sql);
 
+?>
 
+<?php
+include './connect.php';
+if (isset($_POST["submitlist"])) {
+    $sql = "insert into listmusic(NameList) values ('$_POST[namelist]')";
+    if (mysqli_query($conn, $sql)) {
+        echo "danh sách tạo thành công.";
+    } else {
+        echo "Lỗi: " . $sql . "<br>" . $conn->error;
+    }
+}
 
+$sql = "select NameList from listmusic";
+$printlist = mysqli_query($conn, $sql);
 ?>
 
 <!DOCTYPE html>
@@ -36,7 +47,7 @@ $results = mysqli_query($conn, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="stylesheet" type="text/css" href="main.scss">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Main music</title>
 </head>
@@ -62,13 +73,21 @@ $results = mysqli_query($conn, $sql);
             </div>
         </div>
         <div class="container-fluid main row">
-            <div class="container-fluid library d-flex col-md-2 pt-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-book-fill " viewBox="0 0 16 16">
-                    <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
-                </svg>
-                <h2>Thư Viện</h2>
-                <button class="material-icons" id="show" onclick="Show()">add</button>
-
+            <div class="container-fluid library col-md-2 pt-1">
+                <div class="title d-flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-book-fill " viewBox="0 0 16 16">
+                        <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
+                    </svg>
+                    <h2>Thư Viện</h2>
+                    <button class="material-icons" id="show" onclick="Show()">add</button>
+                </div>
+                <div class="listmusic">
+                    <?php
+                    foreach ($printlist as $print) {
+                        echo $print['NameList'] . "<br>";
+                    }
+                    ?>
+                </div>
             </div>
 
             <div class="list col-md-10">
@@ -86,31 +105,6 @@ $results = mysqli_query($conn, $sql);
                         echo '</div>';
                     }
                     ?>
-                    <!-- <div class="col-sm">
-                    <img src="./img/cut you off.jpg" alt="" width="100" height="100">
-                    <p>cut you off</p>
-                    <p>CHINCHILLA</p>
-                </div> -->
-                    <!-- <div class="col-sm">
-                    <img src="./img/cut you off.jpg" alt="" width="100" height="100">
-                    <p>cut you off</p>
-                    <p>CHINCHILLA</p>
-                </div>
-                <div class="col-sm">
-                    <img src="./img/cut you off.jpg" alt="" width="100" height="100">
-                    <p>cut you off</p>
-                    <p>CHINCHILLA</p>
-                </div>
-                <div class="col-sm">
-                    <img src="./img/cut you off.jpg" alt="" width="100" height="100">
-                    <p>cut you off</p>
-                    <p>CHINCHILLA</p>
-                </div>
-                <div class="col-sm">
-                    <img src="./img/cut you off.jpg" alt="" width="100" height="100">
-                    <p>cut you off</p>
-                    <p>CHINCHILLA</p>
-                </div> -->
                 </div>
             </div>
         </div>
@@ -120,12 +114,17 @@ $results = mysqli_query($conn, $sql);
         <button id="AddList" onclick="AddList()">thêm danh sách</button>
     </div>
     <form class="addmusic" id="showcreate" style="display:none;" class="container-fluid" action="main.php" method="post" enctype="multipart/form-data">
-            <label for="image">Chọn hình ảnh:</label>
-            <input type="file" name="image"></br>
-            <label for="artist">Tên ca sĩ:</label>
-            <input type="text" name="artist">
-            <input type="submit" name="submit" value="Tải lên">
-        </form>
+        <label for="image">Chọn hình ảnh:</label>
+        <input type="file" name="image"></br>
+        <label for="artist">Tên ca sĩ:</label>
+        <input type="text" name="artist">
+        <input type="submit" name="submitmusic" value="Tải lên">
+    </form>
+    <form class="addlist" id="showList" style="display:none;" class="container-fluid" action="main.php" method="post" enctype="multipart/form-data">
+        <label for="namelist">Tên danh sách:</label>
+        <input type="text" name="namelist">
+        <input type="submit" name="submitlist" value="Tạo">
+    </form>
 </body>
 
 <script>
@@ -136,15 +135,15 @@ $results = mysqli_query($conn, $sql);
         if (clicked) {
             document.getElementById('create').style.display = "none";
         }
-            
+
     })
 
     function Show() {
         document.getElementById('create').style.display = "block";
         document.getElementById('create').style.position = "absolute";
-        if(clicked)
-            clicked=false;
-        else clicked=true;
+        if (clicked)
+            clicked = false;
+        else clicked = true;
 
     }
 
@@ -153,10 +152,11 @@ $results = mysqli_query($conn, $sql);
         document.getElementById('showcreate').style.position = "absolute";
         document.getElementById('showcreate').style.zIndex = "1";
     }
-    // document.addEventListener("click", function() {
-    //     document.getElementById('create').style.display = "block";
-    //     alert("Bạn đã click vào màn hình");
-    // });
+    function AddList(){
+        document.getElementById('showList').style.display = "block";
+        document.getElementById('showList').style.position = "absolute";
+        document.getElementById('showList').style.zIndex = "1";
+    }
 </script>
 
 </html>
