6f890493bb7913963ad22755228ba1b93645f51e
TQVLucKy <tranquanvinh882003@gmail.com>
2024-02-02 11:27:08 +0700

    synchronized media

--- playmusic.php
+++ playmusic.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="playmusic.scss">
+<link rel="stylesheet" type="text/css" href="playmusic.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
 
 
@@ -9,19 +9,31 @@
     </div>
     <div class="container">
         <div class="box-disk">
-
         </div>
-        <span class="current-music">tên bài</span>
-        <span class="current-artist">ca sĩ</span>
+        <div class="title">
+            <span class="current-time">00:00</span>
+            <span class="music-time">00:00</span>
+            <input type="range" value="0" class="seek-bar" />
+            <span class="current-music">tên bài</span>
+            <span class="current-artist">ca sĩ</span>
+            <div class="controls">
+                <button class="btn btnback"><i class="fa fa-chevron-left"></i></button>
+                <button class="btn-play pause">
+                    <span></span>
+                    <span></span>
+                </button>
+                <button class="btn btnnext"><i class="fa fa-chevron-right"></i></button>
+            </div>
+        </div>
     </div>
 </div>
 
 <!-- mini music -->
 <audio src="" id="audio"></audio>
-<div class="music-slider">
+<div class="music-slider" id="music-slider" style="display: none;">
     <span class="current-time">00:00</span>
     <span class="music-time">00:00</span>
-    <input type="range" value="0" class="seek-bar"/>
+    <input type="range" value="0" class="seek-bar" />
     <span class="current-music">tên bài</span>
     <span class="current-artist">ca sĩ</span>
     <div class="controls">
@@ -33,30 +45,61 @@
         <button class="btn btnnext"><i class="fa fa-chevron-right"></i></button>
     </div>
 </div>
+
+<img src="img/1702540579.jpg" width="100%" height="100%"></br>
+<img src="img/1702540579.jpg" width="100%" height="100%"></br>
+<img src="img/1702540579.jpg" width="100%" height="100%"></br>
+<img src="img/1702540579.jpg" width="100%" height="100%"></br>
+<img src="img/1702540579.jpg" width="100%" height="100%"></br>
+<img src="img/1702540579.jpg" width="100%" height="100%"></br>
+<img src="img/1702540579.jpg" width="100%" height="100%"></br>
+<img src="img/1702540579.jpg" width="100%" height="100%"></br>
+<img src="img/1702540579.jpg" width="100%" height="100%"></br>
+
+
+
 <script src="song.js"></script>
 <script>
     let currentSong = 0;
 
     const music = document.querySelector('#audio');
-    const seekbar = document.querySelector('.seek-bar');
+    const seekbar = document.querySelectorAll('.seek-bar');
     const artist = document.querySelectorAll('.current-artist');
     const songname = document.querySelectorAll('.current-music');
     const boxdisk = document.querySelector('.box-disk');
     const currenttimes = document.querySelector('.current-time');
     const musictime = document.querySelector('.music-time');
-    const btnplay = document.querySelector('.btn-play');
+    const btnplay = document.querySelectorAll('.btn-play');
     const btnback = document.querySelector('.btnback');
     const btnnext = document.querySelector('.btnnext');
 
+    // window.addEventListener("scroll", () => {
+    //     if (document.body.scrollTop>200)
+    //         document.getElementById('1').style.display = 'block';
+    //     else
+    //         document.getElementById('1').style.display = 'none';
+    // });
+    window.onscroll = function() {
+        myFunction()
+    };
 
-    btnplay.addEventListener('click', () => {
-        if (btnplay.className.includes('pause')) {
-            music.play();
+    function myFunction() {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            document.getElementById('music-slider').style.display = 'block';
         } else {
-            music.pause();
+            document.getElementById('music-slider').style.display = 'none';
         }
-        btnplay.classList.toggle('pause');
-        boxdisk.classList.toggle('play');
+    }
+    btnplay.forEach(element => {
+        element.addEventListener('click', () => {
+            if (element.className.includes('pause')) {
+                music.play();
+            } else {
+                music.pause();
+            }
+            element.classList.toggle('pause');
+            boxdisk.classList.toggle('play');
+        })
     });
 
     const updateCurrentMusicInfo = () => {
@@ -75,13 +118,13 @@
         updateCurrentMusicInfo();
         music.src = song.path;
         boxdisk.style.backgroundImage = 'url(img/1702540579.jpg)';
-        
+
         currenttimes.innerHTML = '00:00';
-        setTimeout(()=>{
+        setTimeout(() => {
             seekbar.max = music.duration;
-            musictime.innerHTML=formatTimes(music.duration);
-        },300);
-            
+            musictime.innerHTML = formatTimes(music.duration);
+        }, 300);
+
     }
     setSong(0);
 
@@ -97,15 +140,27 @@
     }
 
     //set seek bar
-    setInterval(() => {
-        seekbar.value = music.currentTime;
-        currenttimes.innerHTML = formatTimes(music.currentTime)
-        if (Math.floor(music.currentTime) == Math.floor(seekbar.max))
-            btnnext.click();
-    }, 500);
-    seekbar.addEventListener('change', () => {
-        music.currentTime = seekbar.value;
-    })
+    seekbar.forEach(element => {
+        setInterval(() => {
+            element.value = music.currentTime;
+            currenttimes.innerHTML = formatTimes(music.currentTime)
+            if (Math.floor(music.currentTime) == Math.floor(element.max))
+                btnnext.click();
+        }, 500);
+        element.addEventListener('change', () => {
+            music.currentTime = element.value;
+        })
+    });
+
+    // setInterval(() => {
+    //     seekbar.value = music.currentTime;
+    //     currenttimes.innerHTML = formatTimes(music.currentTime)
+    //     if (Math.floor(music.currentTime) == Math.floor(seekbar.max))
+    //         btnnext.click();
+    // }, 500);
+    // seekbar.addEventListener('change', () => {
+    //     music.currentTime = seekbar.value;
+    // })
 
     const PlayMusic = () => {
         music.play();
@@ -114,25 +169,46 @@
     }
 
     //Next and PreView
+    btnnext.forEach(element => {
+        element.addEventListener('click', () => {
+            if (currentSong >= songs.length - 1) {
+                currentSong = 0;
+            } else {
+                currentSong++;
+            }
 
-    btnnext.addEventListener('click', () => {
-        if (currentSong >= songs.length - 1) {
-            currentSong = 0;
-        } else {
-            currentSong++;
-        }
-
-        setSong(currentSong);
+            setSong(currentSong);
+        })
     })
-    btnback.addEventListener('click', () => {
-        if (currentSong <= 0) {
-            currentSong = songs.length + 1;
-        } else {
-            currentSong--;
-        }
+    // btnnext.addEventListener('click', () => {
+    //     if (currentSong >= songs.length - 1) {
+    //         currentSong = 0;
+    //     } else {
+    //         currentSong++;
+    //     }
 
-        setSong(currentSong);
-    })
+    //     setSong(currentSong);
+    // })
+    btnback.forEach(element => {
+        element.addEventListener('click', () => {
+            if (currentSong <= 0) {
+                currentSong = songs.length + 1;
+            } else {
+                currentSong--;
+            }
+
+            setSong(currentSong);
+        })
+    });
+    // btnback.addEventListener('click', () => {
+    //     if (currentSong <= 0) {
+    //         currentSong = songs.length + 1;
+    //     } else {
+    //         currentSong--;
+    //     }
+
+    //     setSong(currentSong);
+    // })
 
     //random music
     // btnrandom.addEventListener('click', () => {
