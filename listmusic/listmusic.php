511e912c44cae3e0f18d1f7674c45d5445b05dfe
TQVLucKy <tranquanvinh882003@gmail.com>
2024-02-19 09:31:42 +0700

    layout editing

--- listmusic/listmusic.php
+++ listmusic/listmusic.php
@@ -1,19 +1,45 @@
 <head>
-    <link rel="stylesheet" type="text/css" href="./listmusic/listmusic.scss">
+    <link rel="stylesheet" type="text/css" href="./listmusic/listmusic.css">
 </head>
 <div class="top d-flex justify-content-between mt-2">
     <h2>Danh sách phát</h2>
     <p class="text-secondary">Hiện tất cả</p>
 </div>
 <div class="list-music row">
-    <?php
-    
-    foreach ($results as $result) {
-        echo '<div id="item-list"  class="items col-2 mb-3 mx-3">';
-        echo '<img style="max-width:100%;height:100%" src= ./img/' . $result['nameimage'] . '><br>';
-        echo $result['name'] . "<br>";
-        echo $result['artist'] . "<br>";
-        echo '</div>';
-    }
-    ?>
+    <div class="items col">
+        <?php
+        $count = 0;
+        foreach ($results as $result) {
+            echo '<div class="item col-2 mb-3 mx-3">';
+            echo '<img style="max-width:100%;height:100%" src= ./img/' . $result['nameimage'] . '><br>';
+            echo $result['name'] . "<br>";
+            echo $result['artist'] . "<br>";
+            echo '</div>';
+            $count++;
+            if ($count % 5 == 0) {
+                echo '<div class="w-100"></div>';
+            }
+        }
+        ?>
+    </div>
 </div>
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        const itemList = document.querySelectorAll('.item');
+        itemList.forEach((item, index)=> {
+            item.addEventListener('click', () => {
+                const itemId = index + 1;
+                if (itemId) {
+                    fetch(`get_product_details.php?id=${itemId}`)
+                        .then(Response => Response.json())
+                        .then(data => {
+                            alert('cc');
+                            document.querySelector('detail1').innerHTML = data;
+                        })
+                        .catch(error => console.error('Error:', error));
+                }
+            });
+        });
+
+    });
+</script>
--- main.php
+++ main.php
@@ -58,7 +58,7 @@ $printlist = mysqli_query($conn, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="main.scss">
+    <link rel="stylesheet" type="text/css" href="main.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Main music</title>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -111,7 +111,7 @@ $printlist = mysqli_query($conn, $sql);
             <!-- main -->
             <div class="menu col-md-10">
                 <!-- list music -->
-                <?php
+                <?php 
                 include "./listmusic/listmusic.php";
                 // include "playmusic.php";
                 ?>
@@ -137,27 +137,7 @@ $printlist = mysqli_query($conn, $sql);
         </form>
     </div>
 </body>
-<script>
-    document.addEventListener("DOMContentLoaded", () => {
-        const itemList = document.querySelectorAll('.items');
-        itemList.forEach(item => {
-            item.addEventListener('click', () => {
-                const itemId = item.dataset.id;
-                
-                if (itemId) {
-                    fetch(`get_product_details.php?id=${itemId}`)
-                        .then(Response => Response.text())
-                        .then(data => {
-                            document.getElementsByClassName('detail1')[0].innerHTML = data;
-                            alert('cc');
-                        })
-                        .catch(error => console.error('Error:', error));
-                }
-            });
-        });
-        
-    });
-</script>
+
 <script>
     var create = document.getElementById('create');
     var clicked = true;
